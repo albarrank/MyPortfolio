34cdaa36b93ce353a2b3c0c7cf1a5a81218715fd
Jack Frost <albarrankevin97@gmail.com>
2020-02-01 12:33:06 -0800

    skeleton set up for project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// initialize express to use ejs as view
+app.set("view engine", "ejs");
+
+// sets public to be default for all links in html
+app.use(express.static("public"));
+
+// renders index page for ejs
+app.get("/", function(req, res) {
+	res.render("pages/index");
+});
+
+app.listen(PORT, function() {
+	console.log("server is listening on port: ", PORT);
+});
