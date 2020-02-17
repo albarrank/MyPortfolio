111e19aa7ddaff1a833a7a61250d1dd865b8c999
Jack Frost <albarrankevin97@gmail.com>
2020-02-16 18:30:29 -0800

    changed button trasition based on click

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,4 +2,15 @@ $(document).ready(() => {
 	$(".nav-link").on("click", function() {
 		$(".toggler").click();
 	});
+
+	$(".project-image").on("click", function() {
+		let codeButton = $("#sourceCode");
+		let liveButton = $("#liveProject");
+
+		codeButton.toggleClass("source-code");
+		codeButton.toggleClass("button-up");
+
+		liveButton.toggleClass("live-project");
+		liveButton.toggleClass("button-down");
+	});
 });
