143c98446bdb40783afb816fa6a291a662661ae3
Jack Frost <albarrankevin97@gmail.com>
2020-02-17 19:10:48 -0800

    added routing folder for html routes and potential nodemailer api route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const pageRoutes = require("./routes/htmlRoutes");
 const PORT = process.env.PORT || 5000;
 
 // initialize express to use ejs as view
@@ -9,15 +9,13 @@ app.set("view engine", "ejs");
 // sets public to be default for all links in html
 app.use(express.static("public"));
 
+// allows express to interpret json data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // renders index page for ejs
-app.get("/", function(req, res) {
-	res.render("pages/index");
-});
+app.use("/", pageRoutes);
 
-app.get("/portfolio", function(req, res) {
-	res.render("pages/portfolio");
-});
-
-app.listen(PORT, function() {
+app.listen(PORT, () => {
 	console.log("server is listening on port:", PORT);
 });
