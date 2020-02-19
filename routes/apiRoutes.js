286c0137d407279cd9b63ad7c420f05410db3d0e
Jack Frost <albarrankevin97@gmail.com>
2020-02-18 17:01:19 -0800

    finished adding params for nodemailer through smtp.gmail.com

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,19 @@ const router = express.Router();
 const nodeMailer = require("nodemailer");
 const creds = require("../config/config");
 
+let transporter = nodeMailer.createTransport({
+	host: "smtp.gmail.com",
+	auth: {
+		user: creds.USER,
+		pass: creds.PASS
+	}
+});
+
+transporter.verify((err) => {
+	if (err) console.log(err);
+	else console.log("Server is ready to take emails");
+});
+
 router.post("/email", (req, res) => {
 	const body = req.body;
 
@@ -11,15 +24,7 @@ router.post("/email", (req, res) => {
 	const email = body.email;
 	const message = body.message;
 
-	const content = `<h1>name:</h1> ${name} \n <h1>email:</h1> ${email} \n <h1>message</h1>: ${message} `;
-
-	let transporter = nodeMailer.createTransport({
-		host: "smtp.gmail.com",
-		auth: {
-			user: creds.USER,
-			pass: creds.PASS
-		}
-	});
+	const content = `<h1>name:</h1> <h4>${name}</h4> \n <h1>email:</h1> <h4>${email}</h4> \n <h1>message:</h1> <h4>${message}</h4> `;
 
 	let mailOptions = {
 		from: name,
