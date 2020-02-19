e6d06d37507623abbcd47e8b792da11c20324e5e
Jack Frost <albarrankevin97@gmail.com>
2020-02-18 17:28:52 -0800

    added footer

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -29,17 +29,8 @@ router.post("/email", (req, res) => {
 	if (validEmail) {
 		const content = `<h1>name:</h1> <h4>${name}</h4> \n <h1>email:</h1> <h4>${email}</h4> \n <h1>message:</h1> <h4>${message}</h4> `;
 
-		let mailOptions = {
-			from: name,
-			to: "kevthedev365@gmail.com",
-			subject: "New Message from contact form",
-			html: content
-		};
+		activateNodeMailer(name, content);
 
-		transporter.sendMail(mailOptions, (err, data) => {
-			if (err) console.log("error occured");
-			else console.log("message sent");
-		});
 		res.json({
 			msg: "Email Sent!"
 		});
@@ -48,6 +39,20 @@ router.post("/email", (req, res) => {
 			msg: "Email is Invalid!"
 		});
 });
+
+function activateNodeMailer(name, content) {
+	let mailOptions = {
+		from: name,
+		to: "kevthedev365@gmail.com",
+		subject: "New Message from contact form",
+		html: content
+	};
+
+	transporter.sendMail(mailOptions, (err, data) => {
+		if (err) console.log("error occured");
+		else console.log("message sent");
+	});
+}
 
 function validateEmail(email) {
 	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
