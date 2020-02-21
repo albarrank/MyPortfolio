e550868a85d874750d8ca4018097b02b7bed2c03
Jack Frost <albarrankevin97@gmail.com>
2020-02-21 02:16:00 -0800

    heroku commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
+// depoly through heroku cli
 const PORT = process.env.PORT || 5000;
 
 const pageRoutes = require("./routes/htmlRoutes");
